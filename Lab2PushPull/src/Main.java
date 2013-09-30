c3e7b5a05ceb8677604842c1d3a262793eca065c
Crispy <crpoon@gmail.com>
2013-09-29 21:04:57 -0700

    Added new branch

--- Lab2PushPull/src/Main.java
+++ Lab2PushPull/src/Main.java
@@ -2,17 +2,27 @@
 public class Main {
 	
 	private int x;
+	private int y;
 	
 	public Main() {
-		int x = 0;
+		this.x = 0;
+		this.y = 0;
 	}
 	
 	public int getX(){
 		return this.x;
 	}
 	
+	public int getY(){
+		return this.y;
+	}
+	
 	public void setX(int number){
 		this.x = number;
+	}
+	
+	public void setY(int number){
+		this.y = number;
 	}
 
 }
