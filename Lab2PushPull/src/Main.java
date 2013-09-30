3ca340bc7c3c456707d9c9602d3a86a2821ddb66
Crispy <crpoon@gmail.com>
2013-09-29 20:29:02 -0700

    Initial Push/Commit

---
+++ Lab2PushPull/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	
+	private int x;
+	
+	public Main() {
+		int x = 0;
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public void setX(int number){
+		this.x = number;
+	}
+
+}
